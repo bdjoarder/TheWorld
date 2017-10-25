f5d666b15a0339e071cde2e638c609bfd49cd303
Mobinul Joarder <mobinul.joarder@live.com>
2017-10-24 22:26:35 -0700

    Add project files.

---
+++ TheWorld/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TheWorld.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
