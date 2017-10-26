cb068a6774c3cc49a2f972037850cf417931bc89
Mobinul Joarder <mobinul.joarder@live.com>
2017-10-25 20:45:46 -0700

    Added WorldContext code

--- TheWorld/Data/WorldContext.cs
+++ TheWorld/Data/WorldContext.cs
@@ -1,16 +1,25 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using TheWorld.Models;
 
 namespace TheWorld.Data
 {
     public class WorldContext : DbContext
     {
-        public WorldContext()
-        {
+        private IConfiguration _configuration { get; }
 
+        public WorldContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
         }
 
         public DbSet<Trip> Trips { get; set; }
         public DbSet<Stop> Stops { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer(_configuration[""]);
+        }
     }
 }
